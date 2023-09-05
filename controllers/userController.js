117ee4cfc19acaa63592346ea3e5810e0d9fc72a
molodec1337322 <sshibanov1@yandex.ru>
2023-09-05 19:10:21 +0300

    controllers

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+
+
+async function registration(req, res){
+
+}
+
+async function login(req, res){
+
+}
+
+async function check(req, res){
+
+}
+
+module.exports = {
+    registration,
+    login,
+    check,
+}
---
+++ routes/userRouter.js
@@ -0,0 +1,10 @@
+const Router = require("express")
+const router = new Router()
+const controller = require("../controllers/userController")
+const {registration, login, check} = require("../controllers/userController");
+
+router.post('/registration', registration)
+router.post('/login', login)
+router.get('/auth', check)
+
+module.exports = router
