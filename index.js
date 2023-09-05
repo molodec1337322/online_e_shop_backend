fe7d432ac381c9d2d03c0e15d01542ac12b132b0
molodec1337322 <sshibanov1@yandex.ru>
2023-09-05 17:46:22 +0300

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config()
+const express = require("express")
+const sequelize = require("./db")
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+const start = async () => {
+    try{
+        console.log(process.env.PORT)
+        await sequelize.authenticate()
+        await sequelize.sync()
+
+
+        app.listen(PORT, () => console.log(`Server started on port:${PORT}`))
+    }
+    catch (e){
+        console.log(e)
+    }
+}
+
+start()
