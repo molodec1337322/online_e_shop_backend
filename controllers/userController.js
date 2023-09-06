c42934cd6b94697f74ec712cda8e3b6b881d7410
molodec1337322 <sshibanov1@yandex.ru>
2023-09-06 22:49:08 +0300

    auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,35 @@
 const ApiError = require("../error/ApiError")
+const bcrypt = require("bcrypt")
+const {User, Basket} = require('../models/models')
+const jwt = require("jsonwebtoken")
 
-async function registration(req, res){
+function generateToken(id, email, role){
+    return jwt.sign({
+        id: id,
+        email: email,
+        role: role
+    },
+        process.env.SECRET_KEY,
+        {
+        expiresIn: '24h'
+    })
+}
 
+async function registration(req, res, next){
+    const {email, password, role} = req.body
+    if(!email || !password){
+        return next(ApiError.badRequest("Некорректный догин или пароль"))
+    }
+    const candidate = await User.findOne({where: {email}})
+    if(candidate){
+        return next(ApiError.badRequest("Пользователь с таким Email уже существует"))
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 5)
+    const user = await User.create({email, role, password: hashedPassword})
+    const basket = await Basket.create({userId: user.id})
+    const jwtToken = generateToken(user.id, email, role)
+    return res.json({token: jwtToken})
 }
 
 async function login(req, res){
